9eb28250f13eb7b2685142ea70cd89bcb1a1e56e
jaeiko <jkimkr08@daum.net>
2024-05-05 19:45:33 +0900

    추가: search 페이지 및 search 라우터 설정

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 from flask import Flask, render_template, request, redirect, send_file
 
 # Flask 애플리케이션을 생성합니다.
-app = Flask("__main__")
+app = Flask("JobScraper")
 
 # 홈 페이지를 위한 route를 정의합니다.
+
+
 @app.route("/")
 def home():
     return render_template("home.html")
+
+
+@app.route("/search")
+def search():
+    keyword = request.args.get("keyword")
+    if keyword == None:
+        return redirect("/")
+    return render_template("search.html", keyword=keyword)
+
 
 # Flask 애플리케이션을 실행합니다.
 app.run("0.0.0.0")
