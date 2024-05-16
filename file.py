30ad8e0412e9276f73eec5154a7d9ac8c84a9ad1
jaeiko <jkimkr08@daum.net>
2024-05-16 15:11:28 +0900

    수정:주석처리

--- file.py
+++ file.py
@@ -6,6 +6,7 @@ def save_to_file(file_name, jobs):
     # 문자열 데이터를 파일에 직접 쓰자.
     file.write("Position, Company, Location, URL\n")
 
+    # 파일에 position, company, location, link 열로 해서 데이터 저장
     for job in jobs:
         file.write(
             f"{job['position']}, {job['company']}, {job['location']}, {job['link']}\n"
