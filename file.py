be06771719ebd3b3b3deec61918d82e6ce05d0a9
jaeiko <jkimkr08@daum.net>
2024-06-03 00:24:13 +0900

    수정 : 주석 수정

--- file.py
+++ file.py
@@ -3,7 +3,7 @@ def save_to_file(file_name, jobs):
     # ‘파일 이름’과 ‘파일 열기 모드’를 입력값으로 받고 결괏값으로 파일 객체를 리턴한다.
     file = open(f"{file_name}.csv", "w", encoding="utf-8-sig")
 
-    # 문자열 데이터를 파일에 직접 쓰자.
+    # 문자열 데이터를 파일에 직접 쓴다.
     file.write("Position, Company, Location, URL\n")
 
     # 파일에 position, company, location, link 열로 해서 데이터 저장한다.
